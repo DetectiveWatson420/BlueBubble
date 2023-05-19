5802c206c4e80de42b04824aae1ddf1972290dc8
DetectiveWatson420 <natargabr@charihochargers.org>
2023-05-19 11:54:15 -0400

    Code added to github repo. Hopefully it all works now

--- BlueBubble/src/BlueBubble/Runner.java
+++ BlueBubble/src/BlueBubble/Runner.java
@@ -1,5 +1,25 @@
 package BlueBubble;
 
 public class Runner {
-
+	public static void main(String[] args) {
+		displayBBLogo();
+		surveyTaker mySurvey = new surveyTaker(null);
+	}
+	
+	public static void displayBBLogo() {
+		String logo = "     /\\     /\\\n"
+                + "    /  \\___/  \\\n"
+                + "   /           \\\n"
+                + "  /    Blue     \\\n"
+                + " /    Bubble    \\\n"
+                + "/_______________\\\n"
+                + "\\               /\n"
+                + " \\             /\n"
+                + "  \\           /\n"
+                + "   \\_________/\n";
+    
+		System.out.println(logo);
+		System.out.println("WELCOME, to BlueBubble!"); 
+	}
 }
+
--- BlueBubble/src/BlueBubble/surveyTaker.java
+++ BlueBubble/src/BlueBubble/surveyTaker.java
@@ -1,5 +1,32 @@
 package BlueBubble;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class surveyTaker {
-
+	private ArrayList<String> questions = new ArrayList<String>();
+	private ArrayList<ArrayList<String>> responses = new  ArrayList<ArrayList<String>>();
+	
+	public surveyTaker(ArrayList<String> questions) {
+		askQuestions();
+	}
+	
+	private void askQuestions() {
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("\nLet's begin our survey!");
+		System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("Enter your name!");
+		String userName = myScanner.next();
+		System.out.println("Welcome, " + userName);
+		String more = "y";
+		int questionIndex = 0;
+		for(String question : questions) {
+			while(more.contains("y")) {
+				System.out.println("So, " + userName + " " + question);
+				responses.get(questionIndex).add(myScanner.next());
+				System.out.println("Any other?");
+				more = myScanner.next();
+			}
+			questionIndex++;
+		}
+	}
+	
 }
